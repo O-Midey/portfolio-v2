ef2ec3f9094c058f500112fdf83af8af1d78929a
O-Midey <46810599+O-Midey@users.noreply.github.com>
2025-08-21 14:57:52 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="  w-full md:max-w-[75%] mx-auto overflow-hidden">
+    <main className="min-h-screen w-full md:max-w-[75%] mx-auto overflow-hidden flex items-center">
       <motion.div
-        className="mt-20 px-2 md:px-20 max-w-5xl mx-auto"
+        className="px-2 md:px-20 max-w-5xl mx-auto w-full"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
@@ -21,12 +21,12 @@ export default function Home() {
         </h2>
         <br />
         <p className="text-gray-300 text-base md:text-lg leading-relaxed ">
-          I’m a frontend developer who turns complex ideas into clean,
+          I'm a frontend developer who turns complex ideas into clean,
           responsive interfaces using{" "}
           <span className="text-[#06eecb]">React</span>,{" "}
           <span className="text-[#06eecb]">Next.js</span>, and{" "}
-          <span className="text-[#06eecb]">Typescript</span>. When I’m not
-          debugging codes, you’ll probably find me obsessing over music releases
+          <span className="text-[#06eecb]">Typescript</span>. When I'm not
+          debugging codes, you'll probably find me obsessing over music releases
           or brainstorming wild ideas for marketing rollouts. I like my UIs just
           like my playlists; smooth, engaging, and impossible to skip.
         </p>
@@ -36,7 +36,7 @@ export default function Home() {
         >
           See more about me
           <ArrowRight
-            className="ml-2 transition-transform  space-x-6 "
+            className="ml-2 transition-transform space-x-6"
             size={18}
           />
         </Link>
